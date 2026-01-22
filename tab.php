2150ec06836f506f5048763fbc19df40bc51bda9
ObeyMyGH <babaytg@gmail.com>
2026-01-22 13:11:02 +0300

    Refactor tab.php structure and update links

--- tab.php
+++ tab.php
@@ -25,11 +25,12 @@
  <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0&appId=186162668099890&autoLogAppEvents=1" nonce="HzTTNf9w"></script>
 <div id="fb-root"></div>
 <div class="w-100 d-flex text-left ">
-<div class="fb-like  invert    my-2" data-href="https://shop.zabor-ss.ru/CHARS2SYMBOL/" data-width="" data-layout="standard" data-action="like" data-size="small" data-share="true"></div>
+<div class="fb-like  invert    my-2" data-href="https://izabor.com/CHARS2SYMBOL/" data-width="" data-layout="standard" data-action="like" data-size="small" data-share="true"></div>
 </div>
 
-<div class="fb-comments  bw " data-lazy="true" data-href="https://shop.zabor-ss.ru/CHARS2SYMBOL/"  data-numposts="5"></div>
+<div class="fb-comments  bw " data-lazy="true" data-href="https://izabor.com/CHARS2SYMBOL/"  data-numposts="5"></div>
 </div>
+
 
 
 <iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&targets=%E0%A4%97%E2%AD%AE%CE%A1%E1%8E%BD%E2%B3%9C%E1%88%88%EA%99%94&targets-hint=&default-sum=367&button-text=14&hint=&successURL=&quickpay=shop&account=410015042183703&" width="623" height="222" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
